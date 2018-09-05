339cc62f6d9b5cb8561455e2f01a625ddcb046c5
sstarkey7 <sstarkey@student.cscc.edu>
2018-09-05 16:51:36 -0400

    Added Helloworld

---
+++ src/cscc/edu/Main.java
@@ -0,0 +1,15 @@
+package cscc.edu;
+
+// Name: Steve Starkey
+// Date: September 3, 2018
+// Description:  This is Java's Hello World Program.
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World");
+
+    }
+}
+
